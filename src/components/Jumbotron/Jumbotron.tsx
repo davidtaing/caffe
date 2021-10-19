962e26dcf948c1710335edaf606851fc721177e7
David Taing <adavidtaing@gmail.com>
2021-10-19 22:20:09 +1100

    feat: add inline styling to video component

--- src/components/Jumbotron/Jumbotron.tsx
+++ src/components/Jumbotron/Jumbotron.tsx
@@ -1,10 +1,20 @@
+import { Container } from "semantic-ui-react";
+
 const Jumbotron = () => (
-  <div className="jumbotron">
-    <h2>We Love Coffee</h2>
-    <video autoPlay loop muted width="100%" height="100%">
+  <Container>
+    <video
+      autoPlay
+      loop
+      muted
+      style={{
+        width: "100%",
+        height: "auto",
+        maxHeight: "1080px",
+      }}
+    >
       <source src="/homepage-bg.mp4" type="video/mp4" />
     </video>
-  </div>
+  </Container>
 );
 
 export default Jumbotron;
