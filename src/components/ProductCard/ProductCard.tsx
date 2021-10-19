a2251a5238af196067f7b94e63a763cbe7e88a3a
David Taing <adavidtaing@gmail.com>
2021-10-19 16:36:21 +1100

    refactor: ProductCard out into seperate component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,18 @@
+import { Card, Image } from "semantic-ui-react";
+
+const ProductCard = () => (
+  <Card>
+    <Image src="" wrapped ui={false} alt="coco" />
+    <Card.Content>
+      <Card.Header>Coco</Card.Header>
+      <Card.Meta>$20.00</Card.Meta>
+      <Card.Description>
+        Straight from Columbia. Chocolatey with hints of canned peaches. <br />
+        <br />
+        <em>Available in 200g, 500g and 1kg sizes.</em>
+      </Card.Description>
+    </Card.Content>
+  </Card>
+);
+
+export default ProductCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import Head from "next/head";
-import { Header, Grid, Card, Icon, Image } from "semantic-ui-react";
+import { Header, Grid, Card } from "semantic-ui-react";
 import Navbar from "../components/Navbar/Navbar";
 import Jumbotron from "../components/Jumbotron/Jumbotron";
+import ProductCard from "../components/ProductCard/ProductCard";
 import styles from "../styles/Home.module.css";
 import "semantic-ui-css/semantic.min.css";
 
@@ -25,32 +26,19 @@ const Home: NextPage = () => {
           </div>
           <div className="product-category-container">
             <Header as="h3">Espresso</Header>
-            <Grid columns={3} divided>
-              <Grid.Row>
-                <Grid.Column>
-                  <Card.Group centered>
-                    <Card>
-                      <Image src="" wrapped ui={false} alt="coco" />
-                      <Card.Content>
-                        <Card.Header>Coco</Card.Header>
-                        <Card.Meta>$20.00</Card.Meta>
-                        <Card.Description>
-                          Straight from Columbia. Chocolatey with hints of
-                          canned peaches. <br />
-                          <br />
-                          <em>Available in 200g, 500g and 1kg sizes.</em>
-                        </Card.Description>
-                      </Card.Content>
-                    </Card>
-                  </Card.Group>
-                </Grid.Column>
-                <Grid.Column>Espresso 2</Grid.Column>
-                <Grid.Column>Espresso 3</Grid.Column>
-              </Grid.Row>
-            </Grid>
+            <Card.Group centered>
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+            </Card.Group>
           </div>
           <div className="product-category-container">
             <Header as="h3">Filter</Header>
+            <Card.Group centered>
+              <ProductCard />
+              <ProductCard />
+              <ProductCard />
+            </Card.Group>
           </div>
         </div>
       </main>
